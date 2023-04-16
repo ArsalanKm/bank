df835185ba1f5f22f8cbe694694e0d4e01eb86fa
Araslankarimzad <karimzadarsalan@gmail.com>
2023-04-16 16:54:29 +0330

    Add viper for conifg files

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/ArsalanKm/simple_bank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueris *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadCofig("../..")
+
+	if err != nil {
+		log.Fatal("cannot load config file")
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect tot the database", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/ArsalanKm/simple_bank/api"
 	db "github.com/ArsalanKm/simple_bank/db/sqlc"
+	"github.com/ArsalanKm/simple_bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	config, err := util.LoadCofig(".")
+	if err != nil {
+		log.Fatal("cannot load configs")
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect tot the database", err)
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("Cannot start server", err)
